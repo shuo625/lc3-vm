6ea091768e3bbf53f17624b9d1b076201491c886
shuo625 <qushuo233@gmail.com>
2022-07-08 01:52:47 +0100

    add main func to init a vm to run

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use std::env;
+
+use lc3_vm::{VMErr, VM};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+
+    let mut vm = VM::new(&filename);
+
+    match vm.exec() {
+        Ok(_) => {}
+        Err(err) => match err {
+            VMErr::INVALIDOP => println!("there are invalid op in the program"),
+        },
+    }
+}
